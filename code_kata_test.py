91bf3ac1f380145a779ef866d863a430497d9e53
marialuciap <158782866+marialuciap@users.noreply.github.com>
2024-02-04 19:14:59 +0100

    Upload code_kata file

---
+++ code_kata_test.py
@@ -0,0 +1,19 @@
+from main import mySum
+import pytest
+
+
+def test1():
+    assert 2 == mySum(1,1)
+
+def test3():
+    assert 2 == 2
+
+def test2():
+    assert 3 == mySum(0,3)
+
+
+
+
+
+
+        
